30962ab93fd8dc64bf9d9b5d9bdad22b305495bd
Aarti Lad <120771285+teabytes@users.noreply.github.com>
2023-07-27 12:52:44 +0530

    Add files via upload

--- TE/LP-1/scheduling.cpp
+++ TE/LP-1/scheduling.cpp
@@ -4,6 +4,8 @@
 #include <queue>
 using namespace std;
 
+#define MAX 99
+
 class Process
 {
     string id;
@@ -39,9 +41,10 @@ class Process
 
 class Scheduler
 {
-    int num;
     Process* arr;
-    Process* readyQ;
+    int num;
+    int* remainingTime;
+
 
     public:
 
@@ -73,6 +76,8 @@ class Scheduler
         for (int i=0; i<num; i++) {
             cout<<"\nProcess ID: ";
             cin>>arr[i].id;
+            cout<<"Arrival Time: ";
+            cin>>arr[i].at;
             cout<<"Burst Time: ";
             cin>>arr[i].bt;
             cout<<"Priority: ";
@@ -89,9 +94,9 @@ class Scheduler
 
 
     // used by std::sort()
-    // returns lower value of burst time
-    static bool compareBT(Process x, Process y) {
-        return x.bt < y.bt;
+    // returns lower value of completion time
+    static bool compareCT(Process x, Process y) {
+        return x.ct < y.ct;
     }
 
 
@@ -102,7 +107,7 @@ class Scheduler
     }
 
 
-    // first come first serve
+    // first come first served
     void fcfs() {
         // sorting by arrival time
         sort(arr, arr+num, compareAT);
@@ -126,24 +131,49 @@ class Scheduler
 
 
     // shortest job first (non-preemptive)
-    void sjf() {
-        // sorting by burst time
-        sort(arr, arr+num, compareBT);
+    void sjf()
+    {    
+        // initialize remaining time for all processes
+        remainingTime = new int[num];
 
-        // for 1st process
-        arr[0].ct = arr[0].bt;
-        arr[0].tt = arr[0].bt;
-        arr[0].wt = 0;
-        arr[0].rt = 0;
+        for (int i=0; i<num; i++)
+        {
+            remainingTime[i] = arr[i].bt;
+        }
 
-        // for the rest
-        int completion = arr[0].bt;
-        for (int i=1; i<num; i++) {
-            completion += arr[i].bt;
-            arr[i].ct = completion;
-            arr[i].tt = arr[i].ct - arr[i].at;
-            arr[i].wt = arr[i].tt - arr[i].bt;
-            arr[i].rt = arr[i].wt;
+        int currentTime = 0;
+        int completedTasks = 0;
+        int shortestTask = 0;
+        int minBurst = MAX;
+
+        while (completedTasks != num)
+        {
+            bool taskInput = false;
+            for (int j=0; j<num; j++)
+            {
+                if (arr[j].at <= currentTime && arr[j].bt < minBurst && remainingTime[j]> 0)
+                {
+                    shortestTask = j;
+                    minBurst = arr[j].bt;
+                    taskInput = true;
+                }
+            }
+
+            if (taskInput == false)
+            {
+                currentTime++;
+                continue;
+            }
+
+            currentTime += arr[shortestTask].bt;
+            remainingTime[shortestTask] = 0;
+            minBurst = MAX;
+            completedTasks++;
+            taskInput = false;
+
+            arr[shortestTask].ct = currentTime;
+            arr[shortestTask].tt = arr[shortestTask].ct - arr[shortestTask].at;
+            arr[shortestTask].wt = arr[shortestTask].tt - arr[shortestTask].bt;
         }
     }
 
@@ -151,16 +181,16 @@ class Scheduler
     //shortest remaining time first (preemptive sjf)
     void srtf() {
         // initialize remaining time for all processes
-        int remainingTime[num];
+        remainingTime = new int[num];
+
         for (int i=0; i<num; i++) {
             remainingTime[i] = arr[i].bt;
         }
 
         int currentTime = 0;
         int completedTasks = 0;
-        int minBurst = 999;
-        int finishTime = 0;
-        int shortestTask;
+        int shortestTask = 0;
+        int minBurst = MAX;
         bool active = false;
 
         while (completedTasks != num) {
@@ -179,43 +209,133 @@ class Scheduler
 
             minBurst = --remainingTime[shortestTask];
             if (minBurst == 0) {
-                minBurst = 999;
+                minBurst = MAX;
             }
 
             if (remainingTime[shortestTask] == 0) {
                 completedTasks++;
-                finishTime = currentTime + 1;
                 active = false;
-                arr[shortestTask].wt = finishTime - arr[shortestTask].at - arr[shortestTask].bt;
+                arr[shortestTask].ct = currentTime + 1;
+                arr[shortestTask].tt = arr[shortestTask].ct - arr[shortestTask].at;
+                arr[shortestTask].wt = arr[shortestTask].tt - arr[shortestTask].bt;
+
                 if (arr[shortestTask].wt < 0) {
                     arr[shortestTask].wt = 0;
                 }
             }
-
             currentTime++;
         }
     }
 
 
     // priority (non-preemptive)
-    void priority() {
-        // sorting by priority
-        sort(arr, arr+num, comparePR);
+    void priority()
+    {
+        remainingTime = new int[num];
 
-        // for 1st process
-        arr[0].ct = arr[0].bt;
-        arr[0].tt = arr[0].bt;
-        arr[0].wt = 0;
-        arr[0].rt = 0;
+        for (int i=0; i<num; i++)
+            remainingTime[i] = arr[i].bt;
 
-        // for the rest
-        int completion = arr[0].bt;
-        for (int i=1; i<num; i++) {
-            completion += arr[i].bt;
-            arr[i].ct = completion;
-            arr[i].tt = arr[i].ct - arr[i].at;
-            arr[i].wt = arr[i].tt - arr[i].bt;
-            arr[i].rt = arr[i].wt;
+        int currentTime = 0;
+        int completedTasks = 0;
+        int completionTime = 0;
+        int higherPriority = 0;
+        int remBurst = 0;
+        int maxPriority = MAX;
+        bool taskActive = false;
+
+        while (completedTasks != num)
+        {
+            for (int j=0; j<num; j++)
+            {
+                if(arr[j].at <= currentTime && remainingTime[j] > 0 && arr[j].pr < maxPriority) {
+                    higherPriority = j;
+                    maxPriority = arr[j].pr;
+                    taskActive = true;
+                }
+            }
+
+            if(!taskActive)
+            {
+                currentTime++;
+                continue;
+            }
+
+            currentTime += arr[higherPriority].bt;
+            remainingTime[higherPriority] = 0;
+            maxPriority = MAX;
+            taskActive = false;
+            completedTasks++;
+            
+            arr[higherPriority].ct = currentTime;
+            arr[higherPriority].tt =  arr[higherPriority].ct -  arr[higherPriority].at;
+            arr[higherPriority].wt =  arr[higherPriority].tt -  arr[higherPriority].bt;
+        }
+    }
+
+    // round robin (preemptive by nature)
+    void roundRobin(int tq)
+    {
+        queue<int> readyQ;  // ready queue to hold process indices
+
+        int currentTime = 0;
+        int completedTasks = 0;
+        int curr;  // holds the current process index
+        
+        // initialize remaining time array
+        remainingTime = new int[num]; 
+        for (int i=0; i<num; i++) 
+        {
+            remainingTime[i] = arr[i].bt;
+        }
+        
+        // check for processes that have arrived at time 0 and push their indices to the Ready Queue
+        for (int j=0; j<num; j++)
+        {
+            if (arr[j].at <= currentTime)
+            {
+                readyQ.push(j);
+            }
+        }
+
+        // continue scheduling until all processes are completed
+        while (completedTasks != num)
+        {
+            curr = readyQ.front(); // get the process index from the front of the Ready Queue
+            readyQ.pop(); // remove the process from the front of the Ready Queue
+
+            int counter = 0; // counter to keep track of time quantum spent on the current process
+
+            // execute the process for a time quantum (ts) or its remaining burst time (whichever is smaller)
+            while (counter!=tq && counter!=remainingTime[curr])
+            {
+                currentTime++; // increment the current time
+                counter++; // increment the counter for time spent on the current process
+
+                // check if any new processes have arrived at the current time and add them to the Ready Queue
+                for (int j=0; j<num; j++)
+                {
+                    if (arr[j].at == currentTime)
+                    {
+                        readyQ.push(j);
+                    }
+                }
+            }
+
+            remainingTime[curr] -= counter;  // reduce the remaining burst time of the current process by the time quantum spent on it
+
+            // if the remaining burst time of the current process becomes zero, the process is completed
+            if (remainingTime[curr] == 0)
+            {
+                arr[curr].ct = currentTime;
+                arr[curr].tt = arr[curr].ct - arr[curr].at;
+                arr[curr].wt = arr[curr].tt - arr[curr].bt;
+                completedTasks++;
+            }
+            else
+            {
+                readyQ.push(curr); // if the process is not completed, push it back to the Ready Queue to be scheduled later
+            }
         }
     }
 
@@ -242,15 +362,23 @@ class Scheduler
 
     // displays gantt chart of process
     void ganttChart() {
+        Process displayQ[num];
+
+        // copy arr into new array displayQ
+        copy(arr, arr+num, displayQ);
+
+        // sort displayQ by completion time
+        sort(displayQ, displayQ+num, compareCT);
+
         cout<<"\n------------------------------------\n";
         cout<<"|";
         for (int i=0; i<num; i++) {
-            cout<<"  "<<arr[i].id<<"  |";
+            cout<<"  "<<displayQ[i].id<<"  |";
         }
         cout<<"\n------------------------------------\n";
         cout<<0<<"      ";
         for (int j=0; j<num; j++) {
-            cout<<arr[j].ct<<"     ";
+            cout<<displayQ[j].ct<<"     ";
         }
         cout<<endl;
     }
@@ -272,12 +400,12 @@ class Scheduler
 
 int main() {
     Scheduler obj;
-    int ch;
+    int ch, quantum;
     char ans = 'y';
 
     do
     {
-        cout<<"\nChoose algorithm: \n1. FCFS\n2. SRTF\n3. Priority (Non-Preemptive)\nEnter choice: ";
+        cout<<"\nChoose algorithm: \n1. FCFS\n2. SJF\n3. SRTF\n4. Priority\n5. Round Robin\n\nEnter choice: ";
         cin>>ch;
 
         switch(ch)
@@ -289,13 +417,25 @@ int main() {
 
             case 2:
             obj.getProcess();
-            obj.srtf();
+            obj.sjf();
             break;
 
             case 3:
+            obj.getProcess();
+            obj.srtf();
+            break;
+
+            case 4:
             obj.getPriorityProcess();
             obj.priority();
             break;
+
+            case 5:
+            cout<<"\nEnter time quantum: ";
+            cin>>quantum;
+            obj.getProcess();
+            obj.roundRobin(quantum);
+            break;            
 
             default:
             cout<<"\nInvalid choice!";
