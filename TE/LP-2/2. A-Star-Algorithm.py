79bab7bad715528163bf713c537ae1a19025def1
Aarti Lad <120771285+teabytes@users.noreply.github.com>
2024-01-10 10:21:38 +0530

    Update 2. A-Star-Algorithm.py

--- TE/LP-2/2. A-Star-Algorithm.py
+++ TE/LP-2/2. A-Star-Algorithm.py
@@ -1,57 +1,71 @@
 class AStarAlgorithm:
+
     def __init__(self, start_node, stop_node, graph_nodes, heuristic_dist):
         self.start_node = start_node
         self.stop_node = stop_node
         self.graph_nodes = graph_nodes
         self.heuristic_dist = heuristic_dist
-        self.open_set = set([start_node])
-        self.closed_set = set()
-        self.g = {start_node: 0}
+        self.open_set = set([start_node])  # unvisited
+        self.closed_set = set()  # visited
+        self.g = {start_node: 0}  # cost to reach each node from start_node
         self.parents = {start_node: start_node}
 
-    def run_algorithm(self):
-        while len(self.open_set) > 0:
-            n = self._select_node_to_expand()
 
+    def run_algorithm(self):
+
+        while len(self.open_set) > 0:
+            n = self.select_node_to_expand()
+            
+            # if n is stop_node or has no neighbours
             if n == self.stop_node or self.graph_nodes.get(n) is None:
                 pass
             else:
-                for m, weight in self._get_neighbors(n):
+                for m, weight in self.get_neighbours(n):
                     if m not in self.open_set and m not in self.closed_set:
-                        self._add_to_open_set(m, n, weight)
+                        self.add_to_open_set(m, n, weight)
                     else:
-                        self._update_node_cost(m, n, weight)
+                        self.update_cost(m, n, weight)
 
             if n is None:
-                print('Path does not exist!')
+                print('\nPath does not exist!')
                 return None
 
             if n == self.stop_node:
-                path = self._reconstruct_path()
-                print('Path found: {}'.format(path))
+                path = self.reconstruct_path()
+                print('\nPath found: ', end="")
+                for i in path:
+                    print(i, "-> ", end="")
                 return path
 
-            self._update_sets(n)
+            self.update_sets(n)
 
-        print('Path does not exist!')
+        print('\nPath does not exist!')
         return None
+    
 
-    def _select_node_to_expand(self):
+    # returns node with lowest cost to expand
+    def select_node_to_expand(self):
         n = None
         for v in self.open_set:
             if n is None or self.g[v] + self.heuristic_dist[v] < self.g[n] + self.heuristic_dist[n]:
                 n = v
         return n
 
-    def _get_neighbors(self, v):
+
+    # returns list of neighbours of a node
+    def get_neighbours(self, v):
         return self.graph_nodes.get(v, [])
 
-    def _add_to_open_set(self, m, n, weight):
+
+    # adds a node to open)set 
+    def add_to_open_set(self, m, n, weight):
         self.open_set.add(m)
         self.parents[m] = n
         self.g[m] = self.g[n] + weight
 
-    def _update_node_cost(self, m, n, weight):
+
+    # updates cost of a node if lower cost path is found
+    def update_cost(self, m, n, weight):
         if self.g[m] > self.g[n] + weight:
             self.g[m] = self.g[n] + weight
             self.parents[m] = n
@@ -60,7 +74,15 @@ class AStarAlgorithm:
                 self.closed_set.remove(m)
                 self.open_set.add(m)
 
-    def _reconstruct_path(self):
+    
+    # updates open_set & closed_set after evaluating a node
+    def update_sets(self, n):
+        self.open_set.remove(n)
+        self.closed_set.add(n)
+    
+
+    # returns recontructed path from stop_node to start_node
+    def reconstruct_path(self):
         path = []
         n = self.stop_node
         while self.parents[n] != n:
@@ -70,9 +92,6 @@ class AStarAlgorithm:
         path.reverse()
         return path
 
-    def _update_sets(self, n):
-        self.open_set.remove(n)
-        self.closed_set.add(n)
 
 
 def get_user_input():
@@ -86,13 +105,13 @@ def get_user_input():
         neighbors = [(n.strip(), int(w.strip())) for n, w in map(lambda x: x.split(':'), neighbors)]
         graph_nodes[node] = neighbors
 
-    num_heuristics = int(input("Enter the number of heuristic values: "))
+    num_heuristics = int(input("\nEnter the number of heuristic values: "))
     for _ in range(num_heuristics):
         node = input("Enter the node: ")
         value = int(input("Enter the heuristic value: "))
         heuristic_dist[node] = value
 
-    start_node = input("Enter the start node: ")
+    start_node = input("\nEnter the start node: ")
     stop_node = input("Enter the stop node: ")
 
     return start_node, stop_node, graph_nodes, heuristic_dist
