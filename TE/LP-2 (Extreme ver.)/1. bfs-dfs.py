f9b0a99abc72dc91d77302b59af7792e48e3337f
Aarti Lad <120771285+teabytes@users.noreply.github.com>
2024-05-08 19:44:54 +0530

    Corrected dfsRecursive() and added comments

--- TE/LP-2 (Extreme ver.)/1. bfs-dfs.py
+++ TE/LP-2 (Extreme ver.)/1. bfs-dfs.py
@@ -1,107 +1,120 @@
-"""
-This code implements Depth First Search (DFS) and Breadth First Search (BFS) algorithms for traversing an undirected graph - using recursive and iterative methods.
-"""
-
 from collections import defaultdict, deque
-# using defaultdict will assign blank list [] to keys (nodes) that don't have any value yet
 
 class Graph:
 
     def __init__(self):
         self.graph = defaultdict(list)
 
-    # appends node 'v' in list of neighbours of node 'u' and vice-versa
-    def add_edge(self, u, v):
+
+    def addEdge(self, u, v):
         self.graph[u].append(v)
         self.graph[v].append(u)
 
 
-    def dfs_recursive(self, start):
+    def dfsRecursive(self, start):
         visited = set()
-        traversal = []
 
-        def dfs_util(node, level):
+        def dfs(node, level):
+            # add node to visited set and print
             visited.add(node)
-            print(f"Visited node {node} at level {level}")
+            print(f'Visited node {node} at level {level}.')
 
-            for neighbor in self.graph[node]:
-                if neighbor not in visited:
-                    dfs_util(neighbor, level + 1)
+            # check adjacency list and if any neighbour not visited, go to that node
+            for neighbour in self.graph[node]:
+                if neighbour not in visited:
+                    dfs(neighbour, level+1)
+        
+        dfs(start, 0)
 
-        dfs_util(start, 0)
 
-
-    def dfs_iterative(self, start):
+    def dfsIterative(self, start):
         visited = set()
         stack = [(start, 0)]
 
+        # until stack is empty
         while stack:
             node, level = stack.pop()
+            
+            # if current node is unvisited then visit it
             if node not in visited:
                 visited.add(node)
-                print(f"Visited node {node} at level {level}")
+                print(f'Visited node {node} at level {level}.')
 
-                for neighbor in self.graph[node]:
-                    if neighbor not in visited:
-                        stack.append((neighbor, level + 1))
+                # check adj list and if any neighbour is unvisited, add to stack
+                # check in reverse so that most recently added neighbours are visited first
+                for neighbour in reversed(self.graph[node]):
+                    if neighbour not in visited:
+                        stack.append((neighbour, level+1))
 
 
-    def bfs_recursive(self, start):
+    def bfsIterative(self, start):
         visited = set()
+        queue = deque([(start, 0)])  # use a deque with only popleft() function
 
-        def bfs_util(queue, level):
-            if not queue:
-                return
-            next_queue = []
-            for node in queue:
-                visited.add(node)
-                print(f"Visited node {node} at level {level}")
-                for neighbor in self.graph[node]:
-                    if neighbor not in visited:
-                        next_queue.append(neighbor)
-            bfs_util(next_queue, level + 1)
-
-        bfs_util([start], 0)
-
-
-    def bfs_iterative(self, start):
-        visited = set()
-        queue = deque([(start, 0)])
-
+        # until queue is empty
         while queue:
+            # pop first node and add to visited set
             node, level = queue.popleft()
             visited.add(node)
-            print(f"Visited node {node} at level {level}")
+            print(f'Visited node {node} at level {level}.')
+            
+            # check adj list and if any neighbour is unvisited, add to queue and mark visited
+            for neighbour in self.graph[node]:
+                if neighbour not in visited:
+                    queue.append((neighbour, level+1))
+                    visited.add(neighbour)
 
-            for neighbor in self.graph[node]:
-                if neighbor not in visited:
-                    queue.append((neighbor, level + 1))
-                    visited.add(neighbor)
+    
+    # NOTE: BFS is usually not implemented recursively as it doesn't align with the algorithm's nature
+    def bfsRecursive(self, start):
+        visited = set()
+        initial_queue = [(start)]
 
+        def bfs(queue, level):
+            # return if queue is empty
+            if not queue:  
+                return
+            
+            # this will store neighbours of current level's nodes to be processed next
+            next_queue = []
+
+            for node in queue:
+                visited.add(node)
+                print(f'Visited node {node} at level {level}.')
+
+                # check neighbours of current node and if unvisited, append to next queue
+                for neighbour in self.graph[node]:
+                    if neighbour not in visited:
+                        next_queue.append(neighbour)
+            
+            # after processing current level, call bfs() for next level
+            bfs(next_queue, level+1)
+        
+        bfs(initial_queue, 0)
 
 def main():
-    graph = Graph()
 
-    # node can be string/number
-    edges = input("Enter edges (format: 'node1 node2', type 'done' to finish): ")
+    g = Graph()
+
+    edges = input("Enter edges (format 'node1 node2', type 'done' to finish): ")
     while edges != 'done':
         edge_nodes = edges.split()
-        graph.add_edge(edge_nodes[0], edge_nodes[1])
+        g.addEdge(edge_nodes[0], edge_nodes[1])
         edges = input("Enter edges: ")
-
-    start_node = input("Enter starting node: ")
+    
+    start = input("Enter starting node: ")
 
     print("\nDFS Recursive traversal:")
-    graph.dfs_recursive(start_node)
+    g.dfsRecursive(start)
 
     print("\nDFS Iterative traversal:")
-    graph.dfs_iterative(start_node)
+    g.dfsIterative(start)
 
     print("\nBFS Recursive traversal:")
-    graph.bfs_recursive(start_node)
+    g.bfsRecursive(start)
 
     print("\nBFS Iterative traversal:")
-    graph.bfs_iterative(start_node)
+    g.bfsIterative(start)
 
 
 main()
